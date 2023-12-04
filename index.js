f9e5fd5e4b846e4489ec18f162abd7d36ea85b5d
Ryann Hall <ryannhall@Ryanns-MacBook-Air.local>
2023-12-03 22:22:42 -0500

    linked files added some js and html

---
+++ index.js
@@ -0,0 +1,23 @@
+const freelancers = [
+  {name: 'Dr. Carrot', price: '30', occupation: 'chef'},
+  {name: 'Dr. Sparkle', price: '40', occupation: 'teacher'},
+  {name: 'Dr. Pink', price: '60', occupation: 'artist'},
+  {name: 'Dr. Bizarre', price: '70', occupation: 'developer'}
+];
+
+const occupations = [
+  "chef",
+  "teacher",
+  "artist",
+  "developer"
+];
+
+const names = [
+    "Dr. Carrot",
+    "Dr. Sparkle",
+    "Dr. Pink",
+    "Dr. Bizarre"
+]
+
+
+console.log(freelancers);
