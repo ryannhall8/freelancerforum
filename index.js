a71a2867b6a62579fa39a9d0f6fd01da044787b6
Ryann Hall <ryannhall@Ryanns-MacBook-Air.local>
2023-12-05 00:03:34 -0500

    finished js

--- index.js
+++ index.js
@@ -22,24 +22,45 @@ const names = [
     "Dr. Pink",
     "Dr. Bizarre"
 ]
+function render (){
+    spanCount.innerHTML = freelancers.length;
 
-spanCount.innerHTML = freelancers.length;
+    let sum = 0;
+    freelancers.forEach((freelancer)=> {
+        sum = sum + freelancer.price;
+    });
 
-let sum = 0;
-freelancers.forEach((freelancer)=> {
-    sum = sum + freelancer.price;
-});
+    const average = sum / freelancers.length;
 
-const average = sum / freelancers.length;
+    spanAveragePrice.innerHTML = average.toFixed(2);
 
-spanAveragePrice.innerHTML = average;
+    const html = freelancers.map((freelancer)=> {
+        return `
+        <li>
+        ${freelancer.name} is a ${freelancer.occupation} and cost $${freelancer.price.toFixed(2)} per hour
+        </li>
+        `;
+    }).join('')
 
-const html = freelancers.map((freelancer)=> {
-    return `
-    <li>
-      ${freelancer.name} is a ${freelancer.occupation} and cost $${freelancer.price} per hour
-    </li>
-    `;
-}).join('')
+    ul.innerHTML = html;
+}
 
-ul.innerHTML = html;
+render();
+
+const interval = setInterval(()=> {
+    const nameIndex = Math.floor(Math.random()*names.length);
+    const name = names[nameIndex];
+    const occupationIndex = Math.floor(Math.random()*occupations.length);
+    const occupation = occupations[occupationIndex];
+    const price = Math.ceil(Math.random()*60);
+    const randomFreelancer = {
+        name: name,
+        occupation: occupation,
+        price: price
+    };
+    freelancers.push(randomFreelancer);
+    render();
+    if(freelancers.length === 10){
+        clearInterval(interval);
+    }
+}, 1000)
